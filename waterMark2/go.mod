773cf3002c77665f06535c6e43bcfacec9517d2e
Teeramet.K <92136019+tealerr@users.noreply.github.com>
2023-07-20 11:22:57 +0700

    Add files via upload
    test upload

---
+++ waterMark2/go.mod
@@ -0,0 +1,14 @@
+module example.com/greetings
+
+go 1.20
+
+require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/phpdave11/gofpdi v1.0.14-0.20211212211723-1f10f9844311 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/signintech/gopdf v0.18.0 // indirect
+	github.com/tiechui1994/gopdf v0.0.0-20220723131034-4b11a58b280a // indirect
+	github.com/unidoc/unipdf/v3 v3.47.0 // indirect
+	golang.org/x/image v0.9.0 // indirect
+)
---
+++ waterMark2/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+	"log"
+	"os"
+
+	"github.com/nfnt/resize"
+)
+
+func main() {
+	//Add background image
+	bgImg, err := os.Open("vertical.jpg")
+	if err != nil {
+		log.Fatalf("failed to open: %s", err)
+	}
+	//Decode Background Image
+	decodeBgImg, err := jpeg.Decode(bgImg)
+	if err != nil {
+		log.Fatalf("failed to decode: %s", err)
+	}
+	defer bgImg.Close()
+
+	fmt.Println(decodeBgImg)
+
+	//Add watermark image
+	watermark, err := os.Open("highKycIdCardVerticalWatermark.png")
+	if err != nil {
+		log.Fatalf("failed to open: %s", err)
+	}
+	//Decode Watermark
+	decodeWatermark, err := png.Decode(watermark)
+	if err != nil {
+		log.Fatalf("failed to decode: %s", err)
+	}
+	defer watermark.Close()
+
+	//Resize watermark
+	newHeighVertical := 1350
+	verticalResizedImg := resize.Thumbnail(uint(decodeWatermark.Bounds().Dy()), uint(newHeighVertical), decodeWatermark, resize.Lanczos3)
+
+	// newWidthHorizontal := 1350
+	// horizonResizedImg := resize.Thumbnail(uint(newWidthHorizontal), uint(decodeWatermark.Bounds().Dy()), decodeWatermark, resize.Lanczos3)
+
+	//Set Position vertical
+	offset := image.Pt(200, 0)
+	b := decodeBgImg.Bounds()
+	dupImg := image.NewRGBA(b)
+	draw.Draw(dupImg, b, decodeBgImg, image.ZP, draw.Src)
+	draw.Draw(dupImg, verticalResizedImg.Bounds().Add(offset), verticalResizedImg, image.ZP, draw.Over)
+
+	//Set Position horizontal
+	// offset := image.Pt(20, 400)
+	// b := decodeBgImg.Bounds()
+	// dupImg := image.NewRGBA(b)
+	// draw.Draw(dupImg, b, decodeBgImg, image.ZP, draw.Src)
+	// draw.Draw(dupImg, verticalResizedImg.Bounds().Add(offset), horizonResizedImg, image.ZP, draw.Over)
+
+	//Create Duplicate Image with watermark
+	resultDupImg, err := os.Create("dupVerticalTest.jpg")
+	if err != nil {
+		log.Fatalf("failed to create: %s", err)
+	}
+	jpeg.Encode(resultDupImg, dupImg, &jpeg.Options{jpeg.DefaultQuality})
+	defer resultDupImg.Close()
+}
